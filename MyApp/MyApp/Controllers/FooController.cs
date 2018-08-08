f431983f7887322f4499d2a2b59b8e10e8ade8a1
Marcin Piwowarski <marc.piwowarski@hotmail.com>
2018-08-08 19:35:24 +0100

    many forms which sends subclasses in post methods

---
+++ MyApp/MyApp/Controllers/FooController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class FooController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new FooViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(SubModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Index2(Sub2Model viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(nameof(Index), new FooViewModel(viewModel));
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ MyApp/MyApp/Models/FooViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class FooViewModel
+    {
+        public FooViewModel()
+        {
+            SubModel = new SubModel();
+            Sub2Model = new Sub2Model();
+        }
+
+        public FooViewModel(Sub2Model sub2Model)
+        {
+            SubModel = new SubModel();
+            Sub2Model = sub2Model;
+        }
+
+        [Required]
+        public string Waldo { get; set; }
+
+        public SubModel SubModel { get; set; }
+
+        public Sub2Model Sub2Model { get; set; }
+    }
+
+    public class SubModel
+    {
+        [Required]
+        public string Blah { get; set; }
+    }
+
+    public class Sub2Model
+    {
+        [Required]
+        public string Blah2 { get; set; }
+    }
+}
